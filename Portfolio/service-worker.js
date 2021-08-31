579ca15cbd2aa6c5a5816a02c757c90e94496828
AMmetro <metro.trend.now@gmail.com>
2021-08-31 21:04:39 +0300

    fix deploy3

--- Portfolio/service-worker.js
+++ Portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/port/precache-manifest.ff19a8f92d4599cff38e50007b56405e.js"
+  "/precache-manifest.268cab86dc535a49b7bf2d37d80c0693.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/port/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
